01291acd58dbd672c2fba4c13f4eed7c08e428f4
MarijaTuleva <mtuleva@yahoo.com>
2023-12-11 14:33:45 +0100

    added javascript homework

--- JavaScript Homeworks/Homework from 07.12.23 Marija/Homework 3/script.js
+++ JavaScript Homeworks/Homework from 07.12.23 Marija/Homework 3/script.js
@@ -2,7 +2,13 @@ console.log('Homework 3');
 
 let money = 10000;
 let withdrawMoney = parseInt(prompt("Welcome to the ATM.\n You have " + String(money) + " denars in your account.\n How much money would you like to withdraw?"));
-let moneyLeft = money - withdrawMoney;
+
+let moneyLeft = requestMoney();
+
+function requestMoney(){
+    result = money - withdrawMoney;
+    return result;
+}
 
 if (withdrawMoney <= money) {   
    alert ("You have withdrawn " + String(withdrawMoney) + " denars. You now have " + String(moneyLeft) + " denars left in your account.");   
